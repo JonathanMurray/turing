17b182dea943ddcd2da4dab5d2f39153a2733c24
jmurray <jonte.murray@gmail.com>
2019-04-29 23:46:49 +0200

    Fix broken commented code

--- src/main/turing/Main.java
+++ src/main/turing/Main.java
@@ -9,9 +9,9 @@ public class Main {
     Machine machine = ExampleMachines.increasingNumberOfOnesDelimitedByZero(tape);
     MachineRunner.runVisually(
         machine, tape, 20, Duration.ofMillis(100), TapeStringType.REGULAR, System.out);
-    //MachineRunner.runForDuration(machine, tape, Duration.ofSeconds(5));
+    //MachineRunner.runForDuration(machine, tape, Duration.ofSeconds(5), System.out);
     //MachineRunner.runInteractively(machine, tape, Duration.ofMillis(1000),
-    //    Duration.ofMillis(250), System.in, TapeStringType.REGULAR);
+    //    Duration.ofMillis(250), System.in, TapeStringType.REGULAR, System.out);
   }
 
 }
